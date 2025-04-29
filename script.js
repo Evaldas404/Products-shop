22e8e38daaa9b19c7652ff848e170508a5b20ea1
Evaldas404 <evaldas.baranausk@gmail.com>
2025-04-29 12:24:14 +0300

    some progress

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
+const productsWrapper = document.getElementById("products-warpper");
+
 const fetchProducts = async () => {
   const response = await fetch(
     `https://681068e327f2fdac24114000.mockapi.io/products`
   );
   const data = await response.json();
-  console.log(data);
   return data;
 };
-fetchProducts();
+
+const buildProducts = (product) => {
+  product.forEach((element) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const title = document.createElement("h2");
+    title.classList.add("title");
+    title.textContent = element.title;
+
+    const price = document.createElement("h2");
+    price.classList.add("price")
+    price.textContent = element.price + "€";
+
+    const img = document.createElement("img");
+    img.src = element.imageUrl;
+
+    card.append(title,price, img);
+    productsWrapper.append(card);
+  });
+};
+
+const productsDisplay = async () => {
+  const product = await fetchProducts();
+  console.log(product);
+  buildProducts(product);
+};
+productsDisplay();
